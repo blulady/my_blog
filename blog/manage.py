a4d93da10abbb96d66b084d3ee64b23602aa676f
ssanger <ssanger2020@gmail.com>
2024-06-30 13:56:39 -0700

    added code to get the correct Debug

--- blog/manage.py
+++ blog/manage.py
@@ -2,11 +2,33 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-from blog.settings.base import DEBUG
+# from blog.settings.base import DEBUG
 
+
+# def main():
+#     """Run administrative tasks."""
+#     if DEBUG:
+#         os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog.settings.local')
+#         print('running from Debug')
+#     else:
+#         os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog.settings.production')
+#     try:
+#         from django.core.management import execute_from_command_line
+#     except ImportError as exc:
+#         raise ImportError(
+#             "Couldn't import Django. Are you sure it's installed and "
+#             "available on your PYTHONPATH environment variable? Did you "
+#             "forget to activate a virtual environment?"
+#         ) from exc
+#     execute_from_command_line(sys.argv)
 
 def main():
     """Run administrative tasks."""
+    try:
+        from blog.settings.local import DEBUG
+    except ImportError:
+        DEBUG = False
+        
     if DEBUG:
         os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog.settings.local')
         print('running from Debug')
