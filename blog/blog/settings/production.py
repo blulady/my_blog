bbd9e2480e34a3b7fffb06e23ed8b8dad33d3f2e
ssanger <ssanger2020@gmail.com>
2024-06-30 18:06:10 -0700

    added varables individually

--- blog/blog/settings/production.py
+++ blog/blog/settings/production.py
@@ -8,8 +8,18 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-DATABASES['default'] = dj_database_url.config(
-    default = os.environ.get('DATABASE_URL'),
-    conn_max_age=500,
-    conn_health_checks=True,
-)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('NAME'),
+        'USER': os.environ.get('USER'),
+        'HOST': os.environ.get('HOST'),
+        'PORT': os.environ.get('PORT'),
+    }
+}
+
+# DATABASES['default'] = dj_database_url.config(
+#     default = os.environ.get('DATABASE_URL'),
+#     conn_max_age=500,
+#     conn_health_checks=True,
+# )
