66a0deb2bc0f480a363723b8250063f0dbbafeaf
ssanger <ssanger2020@gmail.com>
2024-07-16 21:49:03 -0700

    mail sending works locally

--- blog/blog/blog_app/forms.py
+++ blog/blog/blog_app/forms.py
@@ -5,4 +5,5 @@ class ContactForm(forms.Form):
     name = forms.CharField(max_length=250)
     email = forms.EmailField(max_length=250)
     phone = forms.CharField(max_length=11)
+    subject = forms.CharField(max_length=250)
     message = forms.CharField(widget=forms.Textarea)
--- blog/blog/blog_app/views.py
+++ blog/blog/blog_app/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.views.generic import ListView, DetailView
 from .models import Post
+
 from .forms import ContactForm
 
 
@@ -29,13 +31,36 @@ def about_me(request):
 
 def contact(request):
     if request.method == "POST":
-        form = ContactForm(request.POST)
-        if form.is_valid():
-            print(form)
-            return HttpResponseRedirect("/success")
-    else:
-        form = ContactForm()
-    return render(request, 'blog/contact.html', {"form": form})
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+
+        data = {
+            'name': name,
+            'email': email,
+            'subject': subject,
+            'message': message
+        }
+
+        message = """
+        New message: {}
+        
+        From: {}
+        """.format(data['message'], data['email'])
+        send_mail(data['subject'], message,'', ["ssanger2020@gmail.com"], fail_silently=False )
+        return HttpResponseRedirect("/success")
+    return render(request, 'blog/contact.html', {})
+
+# def contact(request):
+#     if request.method == "POST":
+#         form = ContactForm(request.POST)
+#         if form.is_valid():
+#             print(form)
+#             return HttpResponseRedirect("/success")
+#     else:
+#         form = ContactForm()
+#     return render(request, 'blog/contact.html', {"form": form})
 
 
 def success(request):
